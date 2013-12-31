e9163eb85df63631d8eab3b23856837f6d18b452
alex <alex@is-debian>
2013-12-31 11:44:48 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+clean:
+	rm -f ex3
+	rm -f ex4
+	rm -f ex6
+	rm -f ex7
---
+++ ex6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	char empty[] = "";
+	printf("Une chaîne vide : %s", empty);
+	return 0;
+}
