b1e06db31fca83a6d8252da645ebec6be29e2b2c
= <alex@is-debian>
2014-01-27 17:52:09 +0100

    Added a helper python script to debug cref

---
+++ cref/db_dump.py
@@ -0,0 +1,15 @@
+# This script helps me debug cref.c
+from struct import *
+datafile = open("cref.dat", "rb")
+# The file contains 5 integers at the beginning
+# 5 integers are 20 bytes long so we read 20 bytes
+data = datafile.read(20)
+maxrows, ntl_size, args_size, sdesc_size, desc_size = \
+unpack('iiiii', data)
+print("Maxrows : %s" % maxrows)
+print("ntl_size : %s" % ntl_size)
+print("args_size : %s" % args_size)
+print("sdesc_size : %s" % sdesc_size)
+print("desc_size : %s " % desc_size)
+
+# Now let's read the records
