337ca244c90fb42ca03e89221ec85576bd10da6b
Alexandre Le Gros <alexandre.lgr@gmail.com>
2014-05-08 18:22:13 +0200

    An exercise about stdarg

---
+++ ex_va_args.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int *test_varargs(int nbArgs, ...)
+{
+	va_list pa;
+	int *args = malloc(sizeof(int) * nbArgs);
+	int i;
+
+	va_start(pa, nbArgs);
+	for (i = 0; i < nbArgs; i++) {
+		args[i] = va_arg(pa, int);
+	}
+	va_end(pa);
+
+	return args;
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int nbArgs = 3;
+	//int nbArgs = atoi(*argv[1]);
+
+	//printf("Taille de la liste d'arguments : %d\n", nbArgs);
+
+	int *args = test_varargs(nbArgs, 76, 46, 33, 94, 36, 3463);
+
+	for (i = 0; i < nbArgs; i++) {
+		printf("Argument %d : %d\n", i+1, args[i]);
+	}
+
+	return 0;
+}
